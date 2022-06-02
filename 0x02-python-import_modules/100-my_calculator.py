599b4d6277bcc6c2d42e6f7399b110cd627ff5a6
ShagizMan <danielgetaneh2011@gmail.com>
2022-06-02 19:17:55 +0300

    rework

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    '''Handle basic arthimetic operations.'''
-    from calculator_1 import add, sub, mul, div
     import sys
-
-    if len(sys.argv) -1 != 3:
+    from calculator_1 import add, sub, mul, div
+    argv = sys.argv[1:]
+    argv_count = len(argv)
+    operators = ["+", "-", "*", "/"]
+    if argv_count != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-
-    ops = {"+": add, "-": sub, "*": mul, "/": div}
-    if sys.argv[2] not in list(ops.keys()):
+        exit(1)
+    elif sys.argv[2] not in operators:
         print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
-
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
+        exit(1)
+    else:
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+        if sys.argv[2] == "+":
+            print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+        elif sys.argv[2] == "-":
+            print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+        elif sys.argv[2] == "*":
+            print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+        elif sys.argv[2] == "/":
+            print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
