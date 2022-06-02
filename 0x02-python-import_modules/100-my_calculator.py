b1f4d5c5692d42485ad54c9f2a7cc6207a3bdcc7
ShagizMan <danielgetaneh2011@gmail.com>
2022-06-02 11:12:19 +0300

    build my own calculator

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    '''Handle basic arthimetic operations.'''
+    from calculator_1 import add, sub, mul, div
+    import sys
+
+    if len(sys.argv) -1 != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    operators = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(operators.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    print("{} {} {} = {}".format(a, sys.argv[2], b, operators[sys.argv[2]](a, b)))
