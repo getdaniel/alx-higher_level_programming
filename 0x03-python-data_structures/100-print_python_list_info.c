267b98c80d1728076deb433a628d0b9cf2a5a42f
ShagizMan <danielgetaneh2011@gmail.com>
2022-06-06 00:55:20 +0300

    fix the betty code

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -2,14 +2,18 @@
 #include <object.h>
 #include <listobject.h>
 
+/**
+ * print_python_list_info - Prints basic info about Python lists.
+ * @p: A PyObject list.
+ */
 void print_python_list_info(PyObject *p)
 {
 	long int size = PyList_Size(p);
-	int i;
+	int index;
 	PyListObject *obj = (PyListObject *)p;
 
 	printf("[*] Size of the Python List = %li\n", size);
 	printf("[*] Allocated = %li\n", obj->allocated);
-	for (i = 0; i < size; i++)
-		printf("Element %i: %s\n", i, Py_TYPE(obj->ob_item[i])->tp_name);
+	for (index = 0; index < size; index++)
+		printf("Element %i: %s\n", index, Py_TYPE(obj->ob_item[index])->tp_name);
 }
