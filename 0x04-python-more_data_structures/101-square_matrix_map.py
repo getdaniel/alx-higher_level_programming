f065e10e41dbecf4849f43ae423e841561df5abe
ShagizMan <danielgetaneh2011@gmail.com>
2022-06-09 01:32:17 +0300

    fix the code

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
     '''Computes the square of all integers of a matrix using map'''
-    return (list(map(lambda x: list(map(lambda y: y ** 2, x[:])),matrix)))
+    return (list(map(lambda x: list(map(lambda y: y ** 2, x)), matrix)))
