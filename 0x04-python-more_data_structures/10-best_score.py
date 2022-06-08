9b44c2a4469d491cd5907cc2914fc941cda957a1
ShagizMan <danielgetaneh2011@gmail.com>
2022-06-09 00:58:05 +0300

    add a comment

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    '''R`eturns a key with the biggest integer value.'''
     if not a_dictionary:
         return None
     return max(a_dictionary)
