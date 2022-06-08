b45b0a8affa2e59a5d5b1a4134d5aa7eb69a73b5
ShagizMan <danielgetaneh2011@gmail.com>
2022-06-09 01:04:07 +0300

    fixing the error

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,13 +6,13 @@ def roman_to_int(roman_string):
         return (0)
 
     roman_dict = {
-            "I": 1,
-            "V": 5,
-            "X": 10,
-            "L": 50,
-            "C": 100,
-            "D": 500,
-            "M": 1000
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
     }
     num = 0
 
