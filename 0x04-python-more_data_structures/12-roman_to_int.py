03df568d24bd3d33a059f1cd42fc9c1b3a5fa313
ShagizMan <danielgetaneh2011@gmail.com>
2022-06-09 01:06:59 +0300

    change style

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,15 +5,7 @@ def roman_to_int(roman_string):
             roman_string is None):
         return (0)
 
-    roman_dict = {
-            'I': 1,
-            'V': 5,
-            'X': 10,
-            'L': 50,
-            'C': 100,
-            'D': 500,
-            'M': 1000
-    }
+    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     num = 0
 
     for index in range(len(roman_string)):
