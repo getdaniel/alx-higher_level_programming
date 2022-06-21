906450d9cc064c6395327820819301a99e521bcf
ShagizMan <danielgetaneh2011@gmail.com>
2022-06-21 11:00:56 +0300

    fixing

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -2,22 +2,19 @@
 def list_division(my_list_1, my_list_2, list_length):
     '''Divides elements by element 2 lists.'''
     new_list = []
-    count = 0
 
-    while count < list_length:
+    for index in range(list_length):
         try:
-            div = my_list_1[count] / my_list_2[count]
+            div = my_list_1[index] / my_list_2[index]
         except TypeError:
-            div = 0
             print("Wrong type")
-        except ZeroDivisionError:
             div = 0
+        except ZeroDivisionError:
             print("division by 0")
             div = 0
         except IndexError:
-            div = 0
             print("out of range")
+            div = 0
         finally:
-            count += 1
             new_list.append(div)
     return (new_list)
