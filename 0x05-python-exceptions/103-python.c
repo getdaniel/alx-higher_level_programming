89c4951a1c2e9fc9c9c1b803852e7f71c81cc208
ShagizMan <danielgetaneh2011@gmail.com>
2022-06-21 13:10:52 +0300

    style the code

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -6,11 +6,14 @@ void print_python_float(PyObject *p);
 
 /**
  * print_python_list - Prints basic info about Python lists.
+ *
  * @p: A PyObject list object.
+ *
+ * Return: Void.
  */
 void print_python_list(PyObject *p)
 {
-	Py_ssize_t size, alloc, i;
+	Py_ssize_t size, alloc, index;
 	const char *type;
 	PyListObject *list = (PyListObject *)p;
 	PyVarObject *var = (PyVarObject *)p;
@@ -19,43 +22,49 @@ void print_python_list(PyObject *p)
 	alloc = list->allocated;
 
 	fflush(stdout);
-
 	printf("[*] Python list info\n");
+
 	if (strcmp(p->ob_type->tp_name, "list") != 0)
 	{
 		printf("  [ERROR] Invalid List Object\n");
+
 		return;
 	}
 
 	printf("[*] Size of the Python List = %ld\n", size);
 	printf("[*] Allocated = %ld\n", alloc);
 
-	for (i = 0; i < size; i++)
+	for (index = 0; index < size; index++)
 	{
-		type = list->ob_item[i]->ob_type->tp_name;
-		printf("Element %ld: %s\n", i, type);
+		type = list->ob_item[index]->ob_type->tp_name;
+		printf("Element %ld: %s\n", index, type);
+
 		if (strcmp(type, "bytes") == 0)
-			print_python_bytes(list->ob_item[i]);
+			print_python_bytes(list->ob_item[index]);
 		else if (strcmp(type, "float") == 0)
-			print_python_float(list->ob_item[i]);
+			print_python_float(list->ob_item[index]);
 	}
 }
 
 /**
  * print_python_bytes - Prints basic info about Python byte objects.
+ *
  * @p: A PyObject byte object.
+ *
+ * Return: Void.
  */
 void print_python_bytes(PyObject *p)
 {
-	Py_ssize_t size, i;
+	Py_ssize_t size, index;
 	PyBytesObject *bytes = (PyBytesObject *)p;
 
 	fflush(stdout);
-
 	printf("[.] bytes object info\n");
+
 	if (strcmp(p->ob_type->tp_name, "bytes") != 0)
 	{
 		printf("  [ERROR] Invalid Bytes Object\n");
+
 		return;
 	}
 
@@ -68,10 +77,10 @@ void print_python_bytes(PyObject *p)
 		size = ((PyVarObject *)p)->ob_size + 1;
 
 	printf("  first %ld bytes: ", size);
-	for (i = 0; i < size; i++)
+	for (index = 0; index < size; index++)
 	{
-		printf("%02hhx", bytes->ob_sval[i]);
-		if (i == (size - 1))
+		printf("%02hhx", bytes->ob_sval[index]);
+		if (index == (size - 1))
 			printf("\n");
 		else
 			printf(" ");
@@ -80,7 +89,10 @@ void print_python_bytes(PyObject *p)
 
 /**
  * print_python_float - Prints basic info about Python float objects.
+ *
  * @p: A PyObject float object.
+ *
+ * Return: Void.
  */
 void print_python_float(PyObject *p)
 {
