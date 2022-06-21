c267ffbc7c32681cc50ecea6356d491cc0000965
ShagizMan <danielgetaneh2011@gmail.com>
2022-06-21 12:37:20 +0300

    fix pycodestyle

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,6 +5,6 @@ def safe_function(fct, *args):
     try:
         run = fct(*args)
         return (run)
-    except:
-        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+    except Exception as error:
+        print("Exception: {}".format(error), file=sys.stderr)
         return (None)
