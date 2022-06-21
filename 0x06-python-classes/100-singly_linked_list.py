464c8807520ea6ba11c00776badffcbc54691ea9
ShagizMan <danielgetaneh2011@gmail.com>
2022-06-21 21:55:07 +0300

    singly linked list

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+''' Define classes for a singly linked lsit.'''
+
+
+class Node:
+    '''Represent Node class in linked list.'''
+    def __init__(self, data, next_node=None):
+        '''Intialize a new Node class.
+
+        Args:
+           data (int): The data of the new Node.
+           next_node (Node): The next node of the new Node.
+        '''
+        self.data = data
+        self.next_node = next_node
+
+    @property
+    def data(self):
+        '''Get the data of the node.'''
+        return (self.__data)
+
+    @data.setter
+    def data(self, value):
+        if not isinstance(value, int):
+            raise TypeError("data must be an integer")
+        self.__data = value
+
+    @property
+    def next_node(self):
+        '''Get the next_node of the node.'''
+        return (self.__next_node)
+
+    def next_node(self, value):
+        if not isinstance(value, Node) and value is not None:
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
+
+
+class SinglyLinkedList:
+    '''Represent SinglyLinkedList in linked list.'''
+    def __init__(self):
+        '''Initialize a new Linked list class.'''
+        self.__head = None
+
+    def sorted_insert(self, value):
+        '''Insert a new node to the linked list.
+
+        Args:
+           value (Node): The new Node to insert.
+        '''
+        new = Node(value)
+        if self.__head is None:
+            new.next_node = None
+            self.__head = new
+        elif self.__head.data > value:
+            new.next_node = self.__head
+            self.__head = new
+        else:
+            tmp = self.__head
+            while (tmp.next_node is not None and
+                    tmp.next_node.data < value):
+                tmp = tmp.next_node
+            new.next_node = tmp.next_node
+            tmp.next_node = new
+
+    def __str__(self):
+        '''Define the print() representation of a linked list.'''
+        values = []
+        tmp = self.__head
+        while tmp is not None:
+            values.append(str(tmp.data))
+            tmp = tmp.next_node
+        return ('\n'.join(values))
