0a0352407cc2df91b6330a0d06e5f92b744149c1
ShagizMan <danielgetaneh2011@gmail.com>
2022-06-30 05:44:50 -0700

    final for the time

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,14 +1,3 @@
 #!/usr/bin/python3
-''' Defines a copy list function.'''
-
-
 def copy_list(l):
-    ''' Copies old list to a new one.
-
-    Args:
-        l (list): The list to be copied.
-    Returns:
-        A new copied list of the old list.
-    '''
-
     return l.copy()
