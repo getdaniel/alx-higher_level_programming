6ee77575ac44984be2b9e76c5ac91970c4732cf8
ShagizMan <danielgetaneh2011@gmail.com>
2022-08-09 21:18:45 +0300

    don't list without name value

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,6 @@
 -- Lists all records of the table second_table of the database
 -- hbtn_0c_0 in your MySQL server.
-SELECT score, name FROM second_table ORDER BY score DESC;
+SELECT score, name
+FROM second_table
+WHERE name != ""
+ORDER BY score DESC;
