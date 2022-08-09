92cc44778e0d84069ffe580fdd9c46d5d2070472
ShagizMan <danielgetaneh2011@gmail.com>
2022-08-09 20:40:11 +0300

    change value to values

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,7 +1,7 @@
 -- Creates a table second_table in the database hbtn_0c_0
 -- in your MySQL server and add multiples rows.
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
-INSERT INTO second_table (id, name, score) VALUE(1, "John", 10);
-INSERT INTO second_table (id, name, score) VALUE(1, "Alex", 3);
-INSERT INTO second_table (id, name, score) VALUE(1, "Bob", 14);
-INSERT INTO second_table (id, name, score) VALUE(1, "George", 8);
+INSERT INTO second_table (id, name, score) VALUES(1, "John", 10);
+INSERT INTO second_table (id, name, score) VALUES(1, "Alex", 3);
+INSERT INTO second_table (id, name, score) VALUES(1, "Bob", 14);
+INSERT INTO second_table (id, name, score) VALUES(1, "George", 8);
