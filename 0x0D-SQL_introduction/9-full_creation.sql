834096dbdc4ec9c43596c74e076293c7fb8bbf4c
ShagizMan <danielgetaneh2011@gmail.com>
2022-08-09 20:42:05 +0300

    differ the id

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -2,6 +2,6 @@
 -- in your MySQL server and add multiples rows.
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table (id, name, score) VALUES(1, "John", 10);
-INSERT INTO second_table (id, name, score) VALUES(1, "Alex", 3);
-INSERT INTO second_table (id, name, score) VALUES(1, "Bob", 14);
-INSERT INTO second_table (id, name, score) VALUES(1, "George", 8);
+INSERT INTO second_table (id, name, score) VALUES(2, "Alex", 3);
+INSERT INTO second_table (id, name, score) VALUES(3, "Bob", 14);
+INSERT INTO second_table (id, name, score) VALUES(4, "George", 8);
