43026cfab8d3ae3a3f1e07da1e6417b6121182b1
ShagizMan <danielgetaneh2011@gmail.com>
2022-08-10 20:03:43 +0300

    fix by remove the secode idetified by

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,5 @@
 -- Creates the MySQL server user user_0d_1.
 CREATE USER IF NOT EXISTS `user_0d_1`@`localhost`
-	IDENTIFIED BY `user_0d_1_pwd`;
-GRANT ALL PRIVILEGES ON *.* TO `user_0d_1`@`localhost` 
-	IDENTIFIED BY `user_0d_1_pwd`;
+	IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO `user_0d_1`@`localhost`;
 FLUSH PRIVILEGES;
