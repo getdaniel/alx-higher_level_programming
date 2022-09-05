d38a6656701eb5e9561850caa280f2a019a72171
ShagizMan <danielgetaneh2011@gmail.com>
2022-09-05 13:05:24 +0300

    square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const size = Math.floor(Number(process.argv[2]));
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let square = 0; square < size; square++) {
+    let row = '';
+    for (let column = 0; column < size; column++)
+      row += 'X';
+    console.log(row);
+  }
+}
