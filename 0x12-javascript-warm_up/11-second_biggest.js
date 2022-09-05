71f470f37f91e0923927d795ed4f9ade4609f6b6
ShagizMan <danielgetaneh2011@gmail.com>
2022-09-05 13:53:08 +0300

    fix the semistandard error

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@ if (process.argv.length <= 3) {
   console.log(0);
 } else {
   const args = process.argv.map(Number)
-	.slice(2, process.argv.length)
-	.sort((a, b) => a - b);
+    .slice(2, process.argv.length)
+    .sort((a, b) => a - b);
   console.log(args[args.length - 2]);
 }
