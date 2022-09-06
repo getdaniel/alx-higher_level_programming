056958e40d881345e5e6b3e36bf33c600bef98dd
ShagizMan <danielgetaneh2011@gmail.com>
2022-09-06 13:40:48 +0300

    fix the semistandard error

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 exports.converter = function (base) {
-  return (num => num.toString(base));
+  return num => num.toString(base);
 };
--- 0x13-javascript_objects_scopes_closures/tests/10-main.js
+++ 0x13-javascript_objects_scopes_closures/tests/10-main.js
@@ -7,7 +7,6 @@ console.log(myConverter(2));
 console.log(myConverter(12));
 console.log(myConverter(89));
 
-
 myConverter = converter(16);
 
 console.log(myConverter(2));
