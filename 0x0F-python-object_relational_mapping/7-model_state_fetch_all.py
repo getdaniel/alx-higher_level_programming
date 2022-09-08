9d0eec9f59f6c1fd587b3a38b0b9ef3b964512bd
ShagizMan <danielgetaneh2011@gmail.com>
2022-09-08 21:16:16 +0300

    fix the pycodestye error

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -8,8 +8,8 @@ from model_state import State
 
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-            .format(sys.argv[1], sys.argv[2], sys.argv[3], pool_pre_ping=True))
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
+        sys.argv[1], sys.argv[2], sys.argv[3], pool_pre_ping=True))
     Session = sessionmaker(bind=engine)
     session = Session()
 
--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -18,4 +18,3 @@ if __name__ == "__main__":
         print("Nothing")
     else:
         print("{}: {}".format(state.id, state.name))
-
