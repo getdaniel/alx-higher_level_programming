00e5e7cca5dca897dd4e7e2c2a06b67a1257c0c0
ShagizMan <danielgetaneh2011@gmail.com>
2022-09-08 16:24:50 +0300

    fix the pycode style error

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,7 +4,7 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db = sys.argv[3])
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM `states`")
     [print(state) for state in cur.fetchall()]
