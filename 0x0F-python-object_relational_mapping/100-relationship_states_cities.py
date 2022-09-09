fc9267ffefd181ba38341c93c6a8bcc93eeb85ed
ShagizMan <danielgetaneh2011@gmail.com>
2022-09-09 15:07:22 +0300

    fix the pycodestyle

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-Creates the State “California” with the City “San 
-from the database hbtn_0e_100_usa
+Creates the State “California” with the City “San Francisco” from the database
+hbtn_0e_100_usa
 """
 
 from sys import argv
