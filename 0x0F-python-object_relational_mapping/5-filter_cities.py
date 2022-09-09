052f7f19ebe77718bd76363caa1e2173fd1e4b74
ShagizMan <danielgetaneh2011@gmail.com>
2022-09-09 14:00:26 +0300

    fix the localhost, port and comment

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
-# takes in the name of a state as an argument and lists all cities of that
-# state, using the database hbtn_0e_4_usa.
+"""
+Takes in the name of a state as an argument and lists all cities of that
+state, using the database hbtn_0e_4_usa.
+"""
+
+from sys import argv
 import MySQLdb
-import sys
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3],
+            charset='utf8')
     cur = db.cursor()
     cur.execute("SELECT * FROM `cities` as `cur` \
                 INNER JOIN `states` as `st` \
                 ON `cur`.`state_id` = `st`.`id` \
                 ORDER BY `cur`.`id`")
-    print(", ".join([ct[2] for ct in cur.fetchall() if ct[4] == sys.argv[4]]))
+    print(", ".join([ct[2] for ct in cur.fetchall() if ct[4] == argv[4]]))
