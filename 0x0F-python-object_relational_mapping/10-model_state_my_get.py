bee29d0e8fa246a1c1dacfdcb588593f491ef877
ShagizMan <danielgetaneh2011@gmail.com>
2022-09-09 14:36:49 +0300

    shrink the sys import

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -2,7 +2,7 @@
 """
 Prints the State object with the name passed as argument.
 from the database hbtn_0e_6_usa
- """
+"""
 
 from sys import argv
 from sqlalchemy import create_engine
@@ -11,14 +11,14 @@ from model_state import State
 
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           .format(argv[1], argv[2], argv[3]),
                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
 
     found = False
     for state in session.query(State):
-        if state.name == sys.argv[4]:
+        if state.name == argv[4]:
             print("{}".format(state.id))
             found = True
             break
