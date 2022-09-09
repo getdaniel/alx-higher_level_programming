c6b5196859df34d268ad2b42387551824cdeccc1
ShagizMan <danielgetaneh2011@gmail.com>
2022-09-09 11:09:39 +0300

    fixing unknown err

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,10 +2,10 @@
 # lists all states from the database hbtn_0e_0_usa.
 
 import MySQLdb
-import sys
+from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
     try:
         cur.execute("SELECT * FROM `states`")
