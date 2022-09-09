a0de81cd5de6a27a09305e3e79ea39acc01a7ce5
ShagizMan <danielgetaneh2011@gmail.com>
2022-09-09 11:04:06 +0300

    trying to fix errors

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,26 +1,21 @@
 #!/usr/bin/python3
-""" script that lists all states from the database hbtn_0e_0_usa """
-from sys import argv
+# lists all states from the database hbtn_0e_0_usa.
+
 import MySQLdb
+import sys
+
 if __name__ == "__main__":
-    conn = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3],
-        charset="utf8")
-    cur = conn.cursor()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
     try:
-        stmt = "SELECT * FROM states ORDER BY id ASC"
-        cur.execute(stmt)
-        rtn = cur.fetchall()
+        cur.execute("SELECT * FROM `states`")
+        rstm = cur.fetchall()
     except MySQLdb.Error:
         try:
-            rtn = ("MySQLdb Error")
+            rstm = ("MySQLdb Error")
         except IndexError:
-            rtn = ("MySQLdb Error - IndexError")
-    for i in rtn:
-        print(i)
+            rstm = ("MySQLdb Error - IndexError")
+    for index in rstm:
+        print(index)
     cur.close()
-    conn.close()
+    db.close()
