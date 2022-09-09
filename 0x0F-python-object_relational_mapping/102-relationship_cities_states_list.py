22a2249d14eeaf1a99df08b8b3017e93ddf85793
ShagizMan <danielgetaneh2011@gmail.com>
2022-09-09 15:12:09 +0300

    still change the comment style

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Lists all City objects from the database hbtn_0e_101_usa.
+""" Lists all City objects from the database hbtn_0e_101_usa."""
 
 from sys import argv
 from sqlalchemy import create_engine
@@ -9,10 +9,11 @@ from relationship_city import City
 
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           .format(argv[1], argv[2], argv[3]),
                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
 
     for city in session.query(City).order_by(City.id):
         print("{}: {} -> {}".format(city.id, city.name, city.state.name))
+    session.close()
