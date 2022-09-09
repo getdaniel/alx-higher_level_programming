9d74e09062dbb68862dbc0282954347de0311d89
ShagizMan <danielgetaneh2011@gmail.com>
2022-09-09 13:47:03 +0300

    optimize for the final correction

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,12 +6,12 @@ import MySQLdb
 
 if __name__ == "__main__":
     db = MySQLdb.connect(
-            host="localhost",
+            host='localhost',
             port=3306,
             user=argv[1],
             passwd=argv[2],
             db=argv[3],
-            charset="utf8")
+            charset='utf8')
     cur = db.cursor()
     cur.execute("SELECT * FROM `states` ORDER BY `id` ASC")
     [print(state) for state in cur.fetchall()]
--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -3,16 +3,17 @@
 Lists all states with a name starting with N (upper N) from the
 database hbtn_0e_0_usa.
 """
+
+from sys import argv
 import MySQLdb
-import sys
 
 if __name__ == "__main__":
     db = MySQLdb.connect(
             host='localhost',
             port=3306,
-            user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3],
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3],
             charset='utf8')
     cur = db.cursor()
     cur.execute("SELECT * FROM `states` ORDER BY `id`")
