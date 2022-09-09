3bddb2bbb7138d8c69ce0dba91a6040182f2051e
ShagizMan <danielgetaneh2011@gmail.com>
2022-09-09 15:04:08 +0300

    fix comment style and close session

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-# Creates the State “California” with the City “San Francisco”
-# from the database hbtn_0e_100_usa
+"""
+Creates the State “California” with the City “San 
+from the database hbtn_0e_100_usa
+"""
 
 from sys import argv
 from sqlalchemy import create_engine
@@ -10,7 +12,7 @@ from relationship_city import Base, City
 
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           .format(argv[1], argv[2], argv[3]),
                            pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
@@ -18,3 +20,4 @@ if __name__ == "__main__":
 
     session.add(City(name="San Francisco", state=State(name="California")))
     session.commit()
+    session.close()
