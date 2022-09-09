1211914faad94c160fb01830944ebea7807da541
ShagizMan <danielgetaneh2011@gmail.com>
2022-09-09 11:42:15 +0300

    fixing the errors

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,26 +2,18 @@
 # lists all states from the database hbtn_0e_0_usa.
 
 import MySQLdb
-from sys import argv
+import sys
 
 if __name__ == "__main__":
     db = MySQLdb.connect(
             host='localhost',
             port=3306,
-            user=argv[1],
-            passwd=argv[2],
-            db=argv[3],
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
             charset='utf8')
     cur = db.cursor()
-    try:
-        cur.execute("SELECT * FROM `states` ORDER BY id ASC")
-        rstm = cur.fetchall()
-    except MySQLdb.Error:
-        try:
-            rstm = ("MySQLdb Error")
-        except IndexError:
-            rstm = ("MySQLdb Error - IndexError")
-    for index in rstm:
-        print(index)
+    cur.execute("SELECT * FROM `states`")
+    [print(state) for state in cur.fetchall()]
     cur.close()
     db.close()
--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,7 +5,13 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            charset='utf8')
     cur = db.cursor()
     cur.execute("SELECT * FROM `states` ORDER BY `id`")
     [print(state) for state in cur.fetchall() if state[1][0] == "N"]
