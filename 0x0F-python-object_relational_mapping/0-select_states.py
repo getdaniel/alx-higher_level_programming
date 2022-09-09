88c60eb1f522248b443ebb55c2b2ee5571a25641
ShagizMan <danielgetaneh2011@gmail.com>
2022-09-09 10:44:59 +0300

    fixing unknown error

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
 # lists all states from the database hbtn_0e_0_usa.
+import MySQLdb
+import sys
 
-if __name__ == '__main__':
-
-    import MySQLdb
-    import sys
-
-    db = MySQLdb.connect(host='localhost', port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
+    cur.execute("SELECT * FROM `states`")
+    [print(state) for state in cur.fetchall()]
+    cur.close()
+    db.close()
