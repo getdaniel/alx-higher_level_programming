8a140f4d2a972536f7901cb90af61ebd49282f6d
ShagizMan <danielgetaneh2011@gmail.com>
2022-09-09 13:50:13 +0300

    fixing the code style

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
-# takes in an argument and displays all values in the states table of
-# hbtn_0e_0_usa where name matches the argument.
+"""
+Takes in an argument and displays all values in the states table of
+hbtn_0e_0_usa where name matches the argument.
+"""
+
+from sys import argv
 import MySQLdb
-import sys
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3],
+            charset='utf8')
     cur = db.cursor()
     cur.execute("SELECT *\
-                FROM `states` WHERE BINARY `name` = '{}'".format(sys.argv[4]))
+                FROM `states` WHERE BINARY `name` = '{}'".format(argv[4]))
     [print(state) for state in cur.fetchall()]
