4ad28b1cd9eac912ebe620e10876b97fb4fc00ed
ShagizMan <danielgetaneh2011@gmail.com>
2022-09-09 10:53:32 +0300

    trying to fix unkonwn error

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,12 +1,26 @@
 #!/usr/bin/python3
-# lists all states from the database hbtn_0e_0_usa.
+""" script that lists all states from the database hbtn_0e_0_usa """
+from sys import argv
 import MySQLdb
-import sys
-
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT * FROM `states`")
-    [print(state) for state in cur.fetchall()]
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        charset="utf8")
+    cur = conn.cursor()
+    try:
+        stmt = "SELECT * FROM states ORDER BY id ASC"
+        cur.execute(stmt)
+        rtn = cur.fetchall()
+    except MySQLdb.Error:
+        try:
+            rtn = ("MySQLdb Error")
+        except IndexError:
+            rtn = ("MySQLdb Error - IndexError")
+    for i in rtn:
+        print(i)
     cur.close()
-    db.close()
+    conn.close()
