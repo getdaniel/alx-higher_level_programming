9b779f81c5b4059135bdcbfe621b2bcc7b8e4f1a
ShagizMan <danielgetaneh2011@gmail.com>
2022-09-09 13:57:41 +0300

    comment and localhost, port

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
-# Lists all cities from the database hbtn_0e_4_usa.
+""" Lists all cities from the database hbtn_0e_4_usa."""
+
+from sys import argv
 import MySQLdb
-import sys
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3],
+            charset='utf8')
     cur = db.cursor()
     cur.execute("SELECT `cur`.`id`, `cur`.`name`, `st`.`name` \
                 FROM `cities` as `cur` \
