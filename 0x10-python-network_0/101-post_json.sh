3c678110d52859d65405363cdbb4fd187d4d7cac
ShagizMan <danielgetaneh2011@gmail.com>
2022-09-22 15:27:21 +0300

    cURL

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
-
+curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
