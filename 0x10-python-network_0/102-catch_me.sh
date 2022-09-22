1ee7184a880ec26308d4a6b0fe571b47a3874fb3
ShagizMan <danielgetaneh2011@gmail.com>
2022-09-22 15:48:12 +0300

    change to get method

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response.
-curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id = 98" 0.0.0.0:5000/catch_me
+curl -sL -X GET -H "Origin: HolbertonSchool" -d "user_id = 98" 0.0.0.0:5000/catch_me
