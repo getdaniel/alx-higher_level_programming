b49a21da17bb72fdf6875f1fe5b84d2c933bfdab
ShagizMan <danielgetaneh2011@gmail.com>
2022-09-22 14:36:34 +0300

    remove extra flags

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-curl -s "$1" -X GET -H "X-School-User-Id: 98"
+curl -sH "X-School-User-Id: 98" "$1"
