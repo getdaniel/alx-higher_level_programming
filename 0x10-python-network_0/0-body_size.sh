a1d090036c54fb90c98bf64cd26b0ce9cac774b6
ShagizMan <danielgetaneh2011@gmail.com>
2022-09-22 12:17:25 +0300

    make qc -l 3

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# Script that takes in a URL, sends a request to that URL,
-# and displays the size of the body of the response
-
+# Script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
 curl -s "$1" | wc -c
