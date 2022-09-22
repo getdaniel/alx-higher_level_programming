0cf80d7ece4b6428d3582599f048c94354fe45eb
ShagizMan <danielgetaneh2011@gmail.com>
2022-09-22 12:50:58 +0300

    fix the pycode stye

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,7 +8,7 @@ def find_peak(list_of_integers):
         return None
 
     size = len(list_of_integers)
-    if  size == 1:
+    if size == 1:
         return list_of_integers[0]
     elif size == 2:
         return max(list_of_integers)
