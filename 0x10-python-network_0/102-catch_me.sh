8f8688452521a1455d68e5545251eb3993b19463
ShagizMan <danielgetaneh2011@gmail.com>
2022-09-22 15:39:09 +0300

    change to school

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response.
-curl -sL -X PUT -H "Origin: You got me!" -d "user_id = 98" 0.0.0.0:5000/catch_me
+curl -sL -X PUT -H "Origin: School" -d "user_id = 98" 0.0.0.0:5000/catch_me
