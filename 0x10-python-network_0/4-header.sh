ff8cc1910f62e5be0bea1f33d7f9804707b97428
ShagizMan <danielgetaneh2011@gmail.com>
2022-09-22 14:32:30 +0300

    fixing the correct requirement

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-curl -s "$1" -X GET -H "X-Hello School!-User-Id: 98"
+curl -s "$1" -X GET -H "X-School-User-Id: 98"
