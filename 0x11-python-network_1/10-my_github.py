4b028211af932273f1ce95e15d1d20c740a06fcc
ShagizMan <danielgetaneh2011@gmail.com>
2022-09-23 19:13:35 +0300

    fix the pycodestyle

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -14,5 +14,5 @@ from requests.auth import HTTPBasicAuth
 if __name__ == "__main__":
     auth = HTTPBasicAuth(argv[1], argv[2])
     req = requests.get("https://api.github.com/user", auth=auth)
-    
+
     print(req.json().get("id"))
