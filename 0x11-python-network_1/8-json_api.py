da374b7f0909d5e8541c7a0837d4ef32857eec4e
ShagizMan <danielgetaneh2011@gmail.com>
2022-09-23 18:59:46 +0300

    make ==

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -12,7 +12,7 @@ import requests
 
 
 if __name__ == "__main__":
-    q = argv[1] if len(argv) > 1 else ""
+    q = argv[1] if len(argv) == 1 else ""
     req = requests.post("http://0.0.0.0:5000/search_user", data={'q': q})
 
     try:
