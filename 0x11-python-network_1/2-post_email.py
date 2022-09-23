41a2e1cb5cce16780c21eeb1423929d53959b9b1
ShagizMan <danielgetaneh2011@gmail.com>
2022-09-23 15:32:19 +0300

    fixing the code

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 Script that takes in a URL and an email, sends a POST request to the passed
-URL with the email as a parameter, and displays the body of the response 
+URL with the email as a parameter, and displays the body of the response
 (decoded in utf-8).
 """
 from sys import argv
