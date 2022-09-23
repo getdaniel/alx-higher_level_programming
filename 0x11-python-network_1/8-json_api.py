2504ae58ebcf9ec9e49fff3ca527ece390a8bbe1
ShagizMan <danielgetaneh2011@gmail.com>
2022-09-23 17:37:32 +0300

    search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+
+sage: ./8-json_api.py <letter>
+  - The letter is sent as the value of the variable `q`.
+  - If no letter is provided, sends `q=""`.
+"""
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    letter = ""
+    if len(argv) == 1 else argv[1]
+    payload = {"q": letter}
+    req = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+
+    try:
+        response = req.json()
+        if response == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get(id), response.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
