6a32542351703c3ba6ac3ff42e94a1cef8db6ffc
ShagizMan <danielgetaneh2011@gmail.com>
2022-09-23 14:48:33 +0300

    coding

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+Script that takes in a URL, sends a request to the URL and displays
+the value of the X-Request-Id variable found in the header of the response.
+"""
+
+
+if __name__ == "__main__":
