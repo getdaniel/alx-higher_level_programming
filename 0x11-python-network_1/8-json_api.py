4b5894fef5d1e32dd1446f9c5fa0948362f02b76
ShagizMan <danielgetaneh2011@gmail.com>
2022-09-23 19:57:36 +0300

    remove sys

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -12,7 +12,7 @@ import requests
 
 
 if __name__ == "__main__":
-    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    letter = "" if len(argv) == 1 else argv[1]
     req = requests.post("http://0.0.0.0:5000/search_user", {"q": letter})
 
     try:
