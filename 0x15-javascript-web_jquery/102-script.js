027ae372266af011629a9a0b7eb92cb24a1c2e6e
ShagizMan <danielgetaneh2011@gmail.com>
2022-10-27 17:04:58 +0300

    say hello to everybody

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -1,5 +1,5 @@
 $('document').ready(function () {
-  const url = 'https://www.fourtonfish.com/hellosalut/?';
+  const url = 'https://www.fourtonfish.com/hellosalut/hello/';
   $('INPUT#btn_translate').click(function () {
     $.get(url + $.param({ lang: $('INPUT#language_code').val() }), function (data) {
       $('DIV#hello').html(data.hello);
